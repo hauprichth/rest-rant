fd21a1d501c4d44ef3ddc521eb02cbc1cf5dd3cc
hauprichth <hauprichth@gmail.com>
2022-12-08 15:13:32 -0500

    fixed missing ()

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-require('dotenv').config
+require('dotenv').config()
 const express = require('express')
 const app = express()
+
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 app.get('*', (req, res) => {
-    res.status(404)('<h1>404 page</h1>')
+    res.status(404).send('<h1>404 page</h1>')
 })
 
 app.listen(process.env.PORT)
