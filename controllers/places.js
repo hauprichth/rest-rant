a6f9a5269e89d2c105f8d3e3e6f9b7bcd8ce5e80
hauprichth <hauprichth@gmail.com>
2022-12-11 16:27:54 -0500

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,40 +1,31 @@
 const router = require('express').Router()
-const Places = require('../')
+const places = require('../models/places.js')
 
 //places index page
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '../images/spices.jpg',
-        link: '"https://unsplash.com/@stri_khedonia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"',
-        photographer: 'Alice Pasqual'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '../images/muffins.jpg',
-        link: '"https://unsplash.com/@foodfaithfit?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"',
-        photographer: 'Taylor Kiser'
-    }]
-
-    res.render('places/index', {places})
-    
+        res.render('places/index', {places})
 })
 // form for new places
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+//create a new place
 router.post('/', (req,res) => {
-    console.log(req.body)
-    res.send('POST/places')
+    if(!req.body.pic) {
+        //default image if not added
+        req.body.pic = 'http://placekitten/com/400/400'
+    }
+    if(!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state){
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
-//| POST | /places | Create new place |
 //| GET | /places/:id | Details about a particular place |
 //| PUT | /places/:id | Update a particular place |
 //| GET | /places/:id/edit | Form page for editing an existing place |
---
+++ models/places.js
@@ -0,0 +1,17 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '../images/spices.jpg',
+    link: '"https://unsplash.com/@stri_khedonia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"',
+    photographer: 'Alice Pasqual'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '../images/muffins.jpg',
+    link: '"https://unsplash.com/@foodfaithfit?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"',
+    photographer: 'Taylor Kiser'
+}]
