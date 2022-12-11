980d01dc1b5e8eb24e90a699a5ee691a1370592b
hauprichth <hauprichth@gmail.com>
2022-12-11 16:58:28 -0500

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -24,6 +24,18 @@ router.post('/', (req,res) => {
     }
     places.push(req.body)
     res.redirect('/places')
+    router.get('/:id', (req, res) =>{
+        let id = Number(req.params.id)
+        if (isNaN(id)) {
+            res.render('error404')
+        }
+        else if  (!places[id]) {
+        res.render('error404')
+        }
+        else {
+        res.render('places/show', {place: places[id]})
+        }
+    })
 })
 
 //| GET | /places/:id | Details about a particular place |
