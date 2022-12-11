b2e3e671cc8b71a786881048cc5d7f7909fc7916
hauprichth <hauprichth@gmail.com>
2022-12-11 15:50:21 -0500

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Places = require('../')
 
+//places index page
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -23,5 +24,17 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
     
 })
+// form for new places
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+//| POST | /places | Create new place |
+//| GET | /places/:id | Details about a particular place |
+//| PUT | /places/:id | Update a particular place |
+//| GET | /places/:id/edit | Form page for editing an existing place |
+//| Delete | /places/:id | Delete a particular place
+//| POST | /places/:id/rant | Create a rant (comment) about a particular place |
+//| Delete | /places/:id/rant/:rantId | Delete a rant (comment) about a particular place |
 
 module.exports = router
