0d18b5fb2a467d09e08f43cd292eac01ec3d7cc2
hauprichth <hauprichth@gmail.com>
2022-12-21 14:31:26 -0500

    fixed route error

--- controllers/places.js
+++ controllers/places.js
@@ -1,63 +1,67 @@
-const router = require('express').Router()
-const places = require('../models/places.js')
+const router = require("express").Router();
+const places = require("../models/places.js");
 
 //places index page
-router.get('/', (req, res) => {
-        res.render('places/index', {places})
-})
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
+});
 // form for new places
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
 //create a new place
-router.post('/', (req,res) => {
-    if(!req.body.pic) {
-        //default image if not added
-        req.body.pic = 'http://placekitten/com/400/400'
-    }
-    if(!req.body.city) {
-        req.body.city = 'Anytown'
-    }
-    if (!req.body.state){
-        req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-})
-// access edit page 
-router.get('/:id/edit', (req, res) => {
-       let id = Number(req.params.id)
-       if (isNaN(id)) {
-           res.render('error404')
-       }
-       else if  (!places[id]) {
-       res.render('error404')
-       }
-       else {
-       res.render('places/edit', { place: places[id] })
-       }
-   })
-   // access show page 
-   router.get('/:id/', (req, res) => {
-          let id = Number(req.params.id)
-          if (isNaN(id)) {
-              res.render('error404')
-          }
-          else if  (!places[id]) {
-          res.render('error404')
-          }
-          else {
-          res.render('places/show', { id: id, place: places[id] })
-          }
-      })
-   
+router.post("/", (req, res) => {
+  if (!req.body.pic) {
+    //default image if not added
+    req.body.pic = "http://placekitten/com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
+});
+// access edit page
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id], id: id });
+  }
+});
+// access show page
+router.get("/:id/", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { id: id, place: places[id] });
+  }
+});
 
+router.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  console.log(id)
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.redirect(`/places/${id}`);
+  }
+});
 
-
-//| PUT | /places/:id | Update a particular place |
 //| Delete | /places/:id | Delete a particular place
 //| POST | /places/:id/rant | Create a rant (comment) about a particular place |
 //| Delete | /places/:id/rant/:rantId | Delete a rant (comment) about a particular place |
 
-module.exports = router
+module.exports = router;
--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use('/places', require('./controllers/places'))
 app.use(methodOverride('_method'))
+
+app.use('/places', require('./controllers/places'))
 
 
 app.get('/', (req, res) => {
