754d59e27dcdd9684e4c322504bac1f41664049c
hauprichth <hauprichth@gmail.com>
2022-12-21 19:49:33 -0500

    connected mongoose

--- controllers/places.js
+++ controllers/places.js
@@ -1,67 +1,55 @@
-const router = require("express").Router();
-const places = require("../models/places.js");
+const router = require('express').Router()
+const db = require('../models')
 
-//places index page
-router.get("/", (req, res) => {
-  res.render("places/index", { places });
-});
-// form for new places
-router.get("/new", (req, res) => {
-  res.render("places/new");
-});
+router.get('/', (req, res) => {
+  db.Place.find()
+  .then((places) => {
+    res.render('places/index', { places })
+  })
+  .catch(err => {
+    console.log(err)
+    res.render('error404')
 
-//create a new place
-router.post("/", (req, res) => {
-  if (!req.body.pic) {
-    //default image if not added
-    req.body.pic = "http://placekitten/com/400/400";
-  }
-  if (!req.body.city) {
-    req.body.city = "Anytown";
-  }
-  if (!req.body.state) {
-    req.body.state = "USA";
-  }
-  places.push(req.body);
-  res.redirect("/places");
-});
-// access edit page
-router.get("/:id/edit", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    res.render("places/edit", { place: places[id], id: id });
-  }
-});
-// access show page
-router.get("/:id/", (req, res) => {
-  let id = Number(req.params.id);
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    res.render("places/show", { id: id, place: places[id] });
-  }
-});
+  })
+})
 
-router.put("/:id", (req, res) => {
-  let id = Number(req.params.id);
-  console.log(id)
-  if (isNaN(id)) {
-    res.render("error404");
-  } else if (!places[id]) {
-    res.render("error404");
-  } else {
-    res.redirect(`/places/${id}`);
-  }
-});
+router.post('/', (req, res) => {
+ db.Place.create(req.body)
+ .then(()=>{
+    res.redirect('/places')
+ })
+ .catch( err => {
+    console.log('err', err)
+    res.render('error404')
+ })
+})
 
-//| Delete | /places/:id | Delete a particular place
-//| POST | /places/:id/rant | Create a rant (comment) about a particular place |
-//| Delete | /places/:id/rant/:rantId | Delete a rant (comment) about a particular place |
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
 
-module.exports = router;
+router.get('/:id', (req, res) => {
+  res.send('GET /places/:id stub')
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
--- models/places.js
+++ models/places.js
@@ -1,17 +1,12 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '../images/spices.jpg',
-    link: '"https://unsplash.com/@stri_khedonia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"',
-    photographer: 'Alice Pasqual'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '../images/muffins.jpg',
-    link: '"https://unsplash.com/@foodfaithfit?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"',
-    photographer: 'Taylor Kiser'
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    pic: String,
+    cuisines: {type: String, required: true},
+    city: {type: String, default: 'Anytown'},
+    state: {type: String, default: 'USA'},
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
